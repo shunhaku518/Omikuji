f9dde5c3fb67324ad5b982b78a9a3b744d2dd5e1
shunhaku518 <ln.m.a.anel@gmail.com>
2025-08-25 15:39:12 +0900

    大吉ひくまでプログラム（while文)

--- omikuji/Program.cs
+++ omikuji/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//おみくじ
+string[] omikuji = new string[5];
+omikuji[0] = "0大吉";
+omikuji[1] = "1中吉";
+omikuji[2] = "2吉";
+omikuji[3] = "3凶";
+omikuji[4] = "4大凶";
+
+
+//この書き方でもOK
+//string[] omikuji = { "0大吉", "1中吉", "2吉", "3凶", "4大凶" };
+
+//各おみくじを何回ひいたかを格納する配列
+int[] count = new int[5];
+
+//Randomクラスを実体化して変数randが機能を参照できる
+Random rand = new Random();
+
+//for (int i = 0; i < 100; i++)
+//{
+//    int result = rand.Next(0, 5); //0以上5未満のランダムを取得して変数resultに代入
+
+//    //result番目の値を配列からひっぱってきてコンソールに出す
+//    Console.WriteLine(omikuji[result]);
+
+//    count[result]++; //resultで引いた結果の番号をカウント
+//}
+
+//for (int i = 0; i < 100; i++)
+//{
+//    Console.WriteLine("今" + i + "回目の繰り返しです");
+//}
+
+//大吉をひくまで繰り返すループ
+bool end = false; //終わりフラグ
+while (!end) //endはfalseであるかどうか　→　成立していれば繰り返し
+{
+    int result = rand.Next(0, 5);//0以上5未満のランダムを取得して変数resultに代入
+    //result番目の値を配列からひっぱってきてコンソールに出す
+    Console.WriteLine(omikuji[result]);
+    count[result]++;//resultで引いた結果の番号をカウント
+
+    //もし大吉を引いていたら変数endはtrue
+    if (result == 0)
+    {
+        end = true;
+    }
+}
+
+
+//配列countの結果を順番に発表
+for (int i = 0; i < count.Length; i++)
+{
+    Console.WriteLine(omikuji[i] + "は" + count[i] + "回ひきました");
+}
+
+
+////論理型
+//bool end = false; //終わりフラグ
+
+////いつ終わりがくるかわからないループ
+//while (end == false) //終わりのフラグがこない間は繰り返す
+//{
+//    int dice = rand.Next(1, 7); //1～6のランダム
+//    if(dice == 1)
+//    {
+//        end = true;
+//    }
+//    Console.WriteLine("つづく");
+//}
+
+
--- omikuji/obj/Debug/net8.0/omikuji.AssemblyInfo.cs
+++ omikuji/obj/Debug/net8.0/omikuji.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("omikuji")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1629df54c6e6c4419c4bf35ce486f736d11f8b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("omikuji")]
 [assembly: System.Reflection.AssemblyTitleAttribute("omikuji")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
